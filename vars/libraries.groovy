354000b624ea3f7b617d0f14deb86144382db525
Vikramduddu <91038677+Vikramduddu@users.noreply.github.com>
2024-10-05 09:58:03 +0530

    Update libraries.groovy

--- vars/libraries.groovy
+++ vars/libraries.groovy
@@ -10,4 +10,5 @@ def buildartifact()
 def continuosdelivery(jobname,ip,context)
 {
   sh " scp /var/lib/jenkins/workspace/${jobname}/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
 
