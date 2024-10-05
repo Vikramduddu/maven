9d2ae9655a7f02e591abeeb5006d1cc26bde23b5
Vikramduddu <91038677+Vikramduddu@users.noreply.github.com>
2024-10-05 09:53:14 +0530

    Update libraries.groovy

--- vars/libraries.groovy
+++ vars/libraries.groovy
@@ -7,4 +7,7 @@ def buildartifact()
   {
    sh "mvn package"
   }
+def continuosdelivery(jobname,ip,context)
+{
+  sh " scp /var/lib/jenkins/workspace/${jobname}/target/webapp.war ubuntu@172.31.45.5:/var/lib/tomcat10/webapps/${context}.war"
 
